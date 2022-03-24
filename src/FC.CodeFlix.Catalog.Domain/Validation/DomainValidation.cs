747dae04cc36dd3a9e832667d881c17270f7b648
Douglas Emerick <douglas@emerick.net.br>
2022-03-24 08:22:45 -0300

    Implementado testes de MinLenght

--- src/FC.CodeFlix.Catalog.Domain/Validation/DomainValidation.cs
+++ src/FC.CodeFlix.Catalog.Domain/Validation/DomainValidation.cs
@@ -14,4 +14,10 @@ public class DomainValidation
         if (String.IsNullOrWhiteSpace(target))
             throw new EntityValidationException($"{fieldName} should not be null or empty");
     }
+
+    public static void MinLegth(string target, int minLength, string fieldName)
+    {
+        if (target.Length < minLength)
+            throw new EntityValidationException($"{fieldName} should not be less than {minLength} characters long");
+    }
 }
--- tests/FC.CodeFlix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -4,6 +4,8 @@ using FC.CodeFlix.Catalog.Domain.Validation;
 using FluentAssertions;
 using System;
 using FC.CodeFlix.Catalog.Domain.Exceptions;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace FC.CodeFlix.Catalog.UnitTests.Domain.Validation;
 public class DomainValidationTest
@@ -54,5 +56,50 @@ public class DomainValidationTest
         Action action = () => DomainValidation.NotNullOrEmpty(target, "fieldName");
 
         action.Should().NotThrow();
+    }
+
+    [Theory(DisplayName = nameof(MinLengthThrowWhenLess))]
+    [Trait("Domain", "DomainValidation - Validation")]    
+    [MemberData(nameof(GetValuesSmallerThanMin), parameters: 10)]
+    public void MinLengthThrowWhenLess(string target, int minLength)
+    {
+        Action action = () => DomainValidation.MinLegth(target, minLength, "fieldName");
+
+        action.Should().Throw<EntityValidationException>()
+            .WithMessage($"fieldName should not be less than {minLength} characters long");
+    }
+
+    [Theory(DisplayName = nameof(MinLengthOk))]
+    [Trait("Domain", "DomainValidation - Validation")]
+    [MemberData(nameof(GetValuesGreaterThanMin), parameters: 10)]
+    public void MinLengthOk(string target, int minLength)
+    {
+        Action action = () => DomainValidation.MinLegth(target, minLength, "fieldName");
+
+        action.Should().NotThrow();
+    }
+
+    public static IEnumerable<object[]> GetValuesSmallerThanMin(int numberOfTests)
+    {
+        yield return new object[] { "123456", 10 };
+        var faker = new Faker();
+        for(int i = 0; i < (numberOfTests - 1); i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var minLength = example.Length + (new Random()).Next(1, 20);
+            yield return new object[] { example, minLength};
+        }
+    }
+
+    public static IEnumerable<object[]> GetValuesGreaterThanMin(int numberOfTests)
+    {
+        yield return new object[] { "123456", 6 };
+        var faker = new Faker();
+        for (int i = 0; i < (numberOfTests - 1); i++)
+        {
+            var example = faker.Commerce.ProductName();
+            var minLength = example.Length - (new Random()).Next(1, 5);
+            yield return new object[] { example, minLength };
+        }
     }
 }
