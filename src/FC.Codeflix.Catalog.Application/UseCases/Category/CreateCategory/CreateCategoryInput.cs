0b2d6c997b6a39725dc2fdf6f1743d7df1600961
Douglas Emerick <douglas@emerick.net.br>
2022-04-01 21:14:52 -0300

    Criado testes para exceptions no use case

--- src/FC.Codeflix.Catalog.Application/UseCases/Category/CreateCategory/CreateCategoryInput.cs
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/CreateCategory/CreateCategoryInput.cs
@@ -2,7 +2,7 @@
 public class CreateCategoryInput
 {
     public string Name { get; set; }
-    public string? Description { get; set; }
+    public string Description { get; set; }
     public bool IsActive { get; set; }
 
     public CreateCategoryInput(string name, string? description = null, bool isActive = true)
--- tests/FC.CodeFlix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -46,6 +46,52 @@ public class CreateCategoryTest
 
     }
 
+    [Fact(DisplayName = nameof(CreateCategoryWithOnlyName))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategoryWithOnlyName()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        var input = new CreateCategoryInput(_fixture.GetValidCategoryName());
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(repository => repository.Insert(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
+        unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().BeEmpty();
+        output.IsActive.Should().BeTrue();
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+    }
+
+    [Fact(DisplayName = nameof(CreateCategoryWithOnlyName))]
+    [Trait("Application", "CreateCategory - Use Cases")]
+    public async void CreateCategoryWithOnlyNameAndDescription()
+    {
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var unitOfWorkMock = _fixture.GetUnitOfWorkMock();
+        var useCase = new UseCases.CreateCategory(repositoryMock.Object, unitOfWorkMock.Object);
+
+        var input = new CreateCategoryInput(_fixture.GetValidCategoryName(), _fixture.GetValidCategoryDescription());
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        repositoryMock.Verify(repository => repository.Insert(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
+        unitOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()), Times.Once);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().BeTrue();
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default(DateTime));
+    }
+
     [Theory(DisplayName = nameof(ThrowWhenCantInstatiateAggregate))]
     [Trait("Application", "CreateCategory - Use Cases")]
     [MemberData(nameof(GetInvalidInputs))]
@@ -62,6 +108,7 @@ public class CreateCategoryTest
 
     private static IEnumerable<object[]> GetInvalidInputs()
     {
+        //
         var fixture = new CreateCategoryTestFixture();
         var invalidInputsList = new List<object[]>();
 
@@ -73,6 +120,7 @@ public class CreateCategoryTest
             "Name should be at leats 3 characters long"
         });
 
+        //
         var invalidInputTooLongName = fixture.GetInput();
         var tooLongNameForCategory = fixture.Faker.Commerce.ProductName();
         while (tooLongNameForCategory.Length <= 255)
@@ -84,6 +132,29 @@ public class CreateCategoryTest
         {
             invalidInputTooLongName,
             "Name should be less 255 characters long"
+        });
+
+        //
+        var invalidInputDescriptionNull = fixture.GetInput();
+        invalidInputDescriptionNull.Description = null;
+        invalidInputsList.Add(new object[]
+        {
+            invalidInputDescriptionNull,
+            "Description should not be null"
+        });
+
+        //
+        var invalidInputTooLongDescription = fixture.GetInput();
+        var tooLongDescriptionForCategory = fixture.Faker.Commerce.ProductDescription();
+        while (tooLongDescriptionForCategory.Length <= 10000)
+            tooLongDescriptionForCategory = $"{tooLongDescriptionForCategory} {fixture.Faker.Commerce.ProductDescription()}";
+
+        invalidInputTooLongDescription.Description = tooLongDescriptionForCategory;
+
+        invalidInputsList.Add(new object[]
+        {
+            invalidInputTooLongDescription,
+            "Description should be less 10000 characters long"
         });
 
         return invalidInputsList;
