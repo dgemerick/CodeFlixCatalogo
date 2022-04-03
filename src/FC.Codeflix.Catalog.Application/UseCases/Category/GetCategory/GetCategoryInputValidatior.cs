08b7bc8c5690825a5291c100e614403cedce2721
Douglas Emerick <douglas@emerick.net.br>
2022-04-03 15:17:46 -0300

    Valicação do input

---
+++ src/FC.Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidatior.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+
+namespace FC.CodeFlix.Catalog.Application.UseCases.Category.GetCategory;
+public class GetCategoryInputValidatior : AbstractValidator<GetCategoryInput>
+{
+    public GetCategoryInputValidatior() => RuleFor(x => x.Id).NotEmpty();
+
+
+}
---
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidationTest.cs
@@ -0,0 +1,28 @@
+﻿using FC.CodeFlix.Catalog.Application.UseCases.Category.GetCategory;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryInputValidationTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+
+    public GetCategoryInputValidationTest(GetCategoryTestFixture fixture) => _fixture = fixture;
+
+    [Fact(DisplayName = nameof(ValidationOk))]
+    [Trait("Application", "GetCategoryInputValidationTest - UseCases")]
+    public void ValidationOk()
+    {
+        var validInput = new GetCategoryInput(Guid.NewGuid());
+        var validator = new GetCategoryInputValidatior();
+
+        var validationResult = validator.Validate(validInput);
+
+        validationResult.Should().NotBeNull();
+        validationResult.IsValid.Should().BeTrue();
+        validationResult.Errors.Should().HaveCount(0);
+    }
+}
