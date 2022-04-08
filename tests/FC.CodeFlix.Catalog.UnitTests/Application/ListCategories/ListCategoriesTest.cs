52081ac576a3252f83abcdc6ad0c8deb0371ab45
Douglas Emerick <douglas@emerick.net.br>
2022-04-08 11:05:34 -0300

    Criado o teste o use case ListCategories

---
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTest.cs
@@ -0,0 +1,56 @@
+﻿using FC.CodeFlix.Catalog.Domain.Entity;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Application.ListCategories;
+
+[Collection(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTest
+{
+    private readonly ListCategoriesTestFixture _fixture;
+
+    public ListCategoriesTest(ListCategoriesTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(List))]
+    [Trait("Application", "ListCategories - use Cases")]
+    public async Task List()
+    {
+        var categoriesExampleList = _fixture.GetExampleCategoriesList();
+        var repositoryMock = _fixture.GetRepositoryMock();
+        var input = ListCategoriesInput(
+            page: 2,
+            perPage: 15,
+            search: "search-example",
+            sort: "name",
+            dir: SearchOrder.Asc
+        );
+
+        repositoryMock.Setup(x => x.Search(
+            It.Is<SearchInput>(
+                searchInput.Page == input.Page &&
+                searchInput.PerPage == input.PerPage &&
+                searchInput.Search == input.Search &&
+                searchInput.OrderBy == input.PerPage &&
+                searchInput.Order == input.Order &&
+            ),
+            It.IsAny<CancellationToken>()
+        )).ReturnsAsync(new OutputSearch<Category>(
+            currentPage: input.Page,
+            perPage: input.PerPage,
+            Items: (IReadOnlyList<Category>)categoriesExampleList,
+            Total: 70
+        ));
+
+        var useCase = new ListCategories(repositoryMock.Object);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        //Asserts
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.UnitTests/Application/ListCategories/ListCategoriesTestFixture.cs
@@ -0,0 +1,58 @@
+﻿using FC.CodeFlix.Catalog.Domain.Entity;
+using FC.CodeFlix.Catalog.Domain.Repository;
+using FC.CodeFlix.Catalog.UnitTests.Common;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.UnitTests.Application.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesTestFixture))]
+public class ListCategoriesTestFixtureCollection : ICollectionFixture<ListCategoriesTestFixture>
+{
+
+}
+
+
+public class ListCategoriesTestFixture : BaseFixture
+{
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();
+
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10000)
+            categoryDescription = categoryDescription[..10000];
+
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean() => (new Random()).NextDouble() < 0.5;
+
+    public Category GetExempleCategory() => new Category(GetValidCategoryName(), GetValidCategoryDescription(), GetRandomBoolean());
+
+    public List<Category> GetExampleCategoriesList(int length = 10)
+    {
+       var list = new List<Category>();
+
+        for (int i = 0; i < length; i++)
+            list.Add(GetExempleCategory());
+
+        return list;
+    }
+}
