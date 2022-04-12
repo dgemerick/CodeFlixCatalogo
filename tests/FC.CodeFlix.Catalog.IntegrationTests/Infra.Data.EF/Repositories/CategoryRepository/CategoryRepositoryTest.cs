2a923118efd8836a2949fcfb4c9fe894b6d179f4
Douglas Emerick <douglas@emerick.net.br>
2022-04-12 20:50:34 -0300

    Criado teste do Insert do repositório CategoryRepository

--- tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -1,8 +1,38 @@
-﻿using Xunit;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
 
 namespace FC.CodeFlix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
 
 [Collection(nameof(CategoryRepositoryTestFixture))]
 public class CategoryRepositoryTest
 {
+    private readonly CategoryRepositoryTestFixture _fixture;
+
+    public CategoryRepositoryTest(CategoryRepositoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(Insert))]
+    [Trait("Integration/Infra.Data", "CategoryRepository - Repositories")]
+    public async Task Insert()
+    {
+        CodeflixCatalogDbContext dbContext = _fixture.CreateDbContext();
+        var exampleCategory = _fixture.GetExampleCategory();
+        var categoryRepository = new CategoryRepository(dbContext);
+
+        await categoryRepository.Insert(exampleCategory, CancellationToken.None);
+        await dbContext.SaveChangesAsync(CancellationToken.None);
+
+        var dbCategory = await dbContext.Categories.Find(exampleCategory.Id);
+
+        dbCategory.Should().NotBeNull();
+        dbCategory.Name.Should().Be(exampleCategory.Name);
+        dbCategory.Description.Should().Be(exampleCategory.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive);
+        dbCategory.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+    }
 }
--- tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -1,4 +1,7 @@
-﻿using FC.CodeFlix.Catalog.IntegrationTests.Base;
+﻿using FC.CodeFlix.Catalog.Domain.Entity;
+using FC.CodeFlix.Catalog.IntegrationTests.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
 using Xunit;
 
 namespace FC.CodeFlix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
@@ -11,4 +14,44 @@ public class CategoryRepositoryTestFixtureCollection : ICollectionFixture<Catego
 
 public class CategoryRepositoryTestFixture : BaseFixture
 {
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10000)
+            categoryDescription = categoryDescription[..10000];
+
+        return categoryDescription;
+    }
+
+    public bool GetRandomBoolean() => new Random().NextDouble() < 0.5;
+
+    public Category GetExampleCategory()
+    => new Category(
+        GetValidCategoryName(),
+        GetValidCategoryDescription(),
+        GetRandomBoolean()
+    );
+
+    public CodeflixCatalogDbContext CreateDbContext()
+    {
+        var dbContext = new CodeflixCatalogDbContext(
+            new DbContextOptionsBuilder<CodeflixCatalogDbContext>()
+                .UseInMemoryDatabase("integration-tests-db")
+                .Options
+        );
+    }
 }
