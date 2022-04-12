d2bcd33bf240bc9295384deb450ed907468b1f7f
Douglas Emerick <douglas@emerick.net.br>
2022-04-12 19:46:47 -0300

    Criado a base para os testes do CategoryRepository

---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTest.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
+
+[Collection(nameof(CategoryRepositoryTestFixture))]
+public class CategoryRepositoryTest
+{
+}
---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Infra.Data.EF/Repositories/CategoryRepository/CategoryRepositoryTestFixture.cs
@@ -0,0 +1,14 @@
+﻿using FC.CodeFlix.Catalog.IntegrationTests.Base;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Infra.Data.EF.Repositories.CategoryRepository;
+
+[CollectionDefinition(nameof(CategoryRepositoryTestFixture))]
+public class CategoryRepositoryTestFixtureCollection : ICollectionFixture<CategoryRepositoryTestFixture>
+{
+
+}
+
+public class CategoryRepositoryTestFixture : BaseFixture
+{
+}
