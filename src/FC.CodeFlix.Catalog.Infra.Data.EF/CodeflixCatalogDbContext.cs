1ef36a78fb208c72241353647b3e5e80b53ff3aa
Douglas Emerick <douglas@emerick.net.br>
2022-04-12 21:05:38 -0300

    Criado o contexto e mapeamentos

---
+++ src/FC.CodeFlix.Catalog.Infra.Data.EF/CodeflixCatalogDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FC.CodeFlix.Catalog.Domain.Entity;
+using FC.CodeFlix.Catalog.Infra.Data.EF.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FC.CodeFlix.Catalog.Infra.Data.EF;
+public class CodeflixCatalogDbContext : DbContext
+{
+    public DbSet<Category> Categories => Set<Category>();
+
+    public CodeflixCatalogDbContext(DbContextOptions<CodeflixCatalogDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new CategoryConfiguration());
+        //builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
