d92607bf857fa90ba8402824fec8f93a1243a776
Douglas Emerick <douglas@emerick.net.br>
2022-04-19 18:47:34 -0300

    Implementado teste integrado do use case GetCategory

---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTest.cs
@@ -0,0 +1,63 @@
+﻿using UseCase = FC.CodeFlix.Catalog.Application.UseCases.Category.GetCategory;
+using FC.CodeFlix.Catalog.Infra.Data.EF.Repositories;
+using System.Threading.Tasks;
+using Xunit;
+using System.Threading;
+using FluentAssertions;
+using System;
+using FC.CodeFlix.Catalog.Application.Exceptions;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryTest
+{
+    private readonly GetCategoryTestFixture _fixture;
+
+    public GetCategoryTest(GetCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(GetCategory))]
+    [Trait("Integration/Application", "GetCategory - Use Cases")]
+    public async Task GetCategory()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var exampleCategory = _fixture.GetExampleCategory();
+        dbContext.Categories.Add(exampleCategory);
+        dbContext.SaveChanges();
+        var repository = new CategoryRepository(dbContext);                
+        var input = new UseCase.GetCategoryInput(exampleCategory.Id);
+        var useCase = new UseCase.GetCategory(repository);
+
+        // act
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        //assert
+        output.Should().NotBeNull();
+        output.Name.Should().Be(exampleCategory.Name);
+        output.Description.Should().Be(exampleCategory.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive);
+        output.Id.Should().Be(exampleCategory.Id);
+        output.CreatedAt.Should().Be(exampleCategory.CreatedAt);
+    }
+
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+    [Trait("Integration/Application", "GetCategory - Use Cases")]
+    public async Task NotFoundExceptionWhenCategoryDoesntExist()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var exampleCategory = _fixture.GetExampleCategory();
+        dbContext.Categories.Add(exampleCategory);
+        dbContext.SaveChanges();
+        var repository = new CategoryRepository(dbContext);
+        var input = new UseCase.GetCategoryInput(Guid.NewGuid());
+        var useCase = new UseCase.GetCategory(repository);
+
+        var task = async () => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>()
+            .WithMessage($"Category '{input.Id}' not found.");
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollection : ICollectionFixture<GetCategoryTestFixture> { }
+
+public class GetCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+}
