22b4f1c3ea20263b700e138d0c6ff6a1e996723f
Douglas Emerick <douglas@emerick.net.br>
2022-04-20 08:46:01 -0300

    Criado teste integrado do use case CreateCategory

---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,49 @@
+﻿using FC.CodeFlix.Catalog.Infra.Data.EF;
+using FC.CodeFlix.Catalog.Infra.Data.EF.Repositories;
+using ApplicationUseCases = FC.CodeFlix.Catalog.Application.UseCases.Category.CreateCategory;
+using System.Threading;
+using Xunit;
+using FluentAssertions;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.CreateCategory;
+
+[Collection(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTest
+{
+    private readonly CreateCategoryTestFixture _fixture;
+
+    public CreateCategoryTest(CreateCategoryTestFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact(DisplayName = nameof(CreateCategory))]
+    [Trait("Integration/Application", "CreateCategory - Use Cases")]
+    public async void CreateCategory()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new ApplicationUseCases.CreateCategory(repository, unitOfWork);
+
+        var input = _fixture.GetInput();
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        var dbCategory = await (_fixture.CreateDbContext(true)).Categories.FindAsync(output.Id);
+
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(input.IsActive);
+        dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTestFixture.cs
@@ -0,0 +1,17 @@
+﻿using FC.CodeFlix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.IntegrationTests.Application.UseCases.Category.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryTestFixture))]
+public class CreateCategoryTestFixtureCollection : ICollectionFixture<CreateCategoryTestFixture> { }
+
+public class CreateCategoryTestFixture : CategoryUseCasesBaseFixture
+{
+    public CreateCategoryInput GetInput()
+    {
+        var category = GetExampleCategory();
+        return new (category.Name, category.Description, category.IsActive);
+    }
+}
