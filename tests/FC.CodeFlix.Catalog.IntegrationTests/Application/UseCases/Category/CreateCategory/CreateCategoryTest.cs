5d784651d0751f797cf22f02da447e4349aaae4e
Douglas Emerick <douglas@emerick.net.br>
2022-04-21 18:50:14 -0300

    Criado teste CreateCategoryOnlyWithNameAndDescription

--- tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTest.cs
+++ tests/FC.CodeFlix.Catalog.IntegrationTests/Application/UseCases/Category/CreateCategory/CreateCategoryTest.cs
@@ -77,4 +77,35 @@ public class CreateCategoryTest
         output.CreatedAt.Should().NotBeSameDateAs(default);
 
     }
+
+    [Fact(DisplayName = nameof(CreateCategoryOnlyWithNameAndDescription))]
+    [Trait("Integration/Application", "CreateCategory - Use Cases")]
+    public async void CreateCategoryOnlyWithNameAndDescription()
+    {
+        var dbContext = _fixture.CreateDbContext();
+        var repository = new CategoryRepository(dbContext);
+        var unitOfWork = new UnitOfWork(dbContext);
+        var useCase = new ApplicationUseCases.CreateCategory(repository, unitOfWork);
+
+        var exampleInput = _fixture.GetInput();
+        var input = new CreateCategoryInput(exampleInput.Name, exampleInput.Description);
+
+        var output = await useCase.Handle(input, CancellationToken.None);
+
+        var dbCategory = await (_fixture.CreateDbContext(true)).Categories.FindAsync(output.Id);
+
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(true);
+        dbCategory.CreatedAt.Should().Be(output.CreatedAt);
+
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(true);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+
+    }
 }
