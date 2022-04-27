966308382a649b27b666bab0ec29389c9dcd18ed
Douglas Emerick <douglas@emerick.net.br>
2022-04-27 20:34:03 -0300

    Criad o Setup dos testes CustomWebApplicationFactory

---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Base/CustomWebApplicationFactory.cs
@@ -0,0 +1,27 @@
+﻿using FC.CodeFlix.Catalog.Infra.Data.EF;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.Base;
+public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var dbOptions = services.FirstOrDefault(x => x.ServiceType == typeof(DbContextOptions<CodeflixCatalogDbContext>));
+
+            if (dbOptions is not null)
+                services.Remove(dbOptions);
+            services.AddDbContext<CodeflixCatalogDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("end2end-test-db");
+            });
+
+        });
+        base.ConfigureWebHost(builder);
+    }
+}
