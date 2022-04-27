922ac8609cf3d2854e71e3640f7e6939a8ddabfc
Douglas Emerick <douglas@emerick.net.br>
2022-04-27 20:16:57 -0300

    Criando o primeiro teste EndToEnd

---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/CreateCategory/CreateCategoryApiTestFixture.cs
@@ -0,0 +1,13 @@
+﻿using FC.CodeFlix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.CodeFlix.Catalog.EndToEndTests.Api.Category.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.Api.Category.CreateCategory;
+
+[CollectionDefinition(nameof(CreateCategoryApiTestFixture))]
+public class CreateCategoryApiTestFixtureCollection : ICollectionFixture<CreateCategoryApiTestFixture> { }
+
+public class CreateCategoryApiTestFixture : CategoryBaseFixture
+{
+    public CreateCategoryInput GetExampleInput() => new CreateCategoryInput(GetValidCategoryName(), GetValidCategoryDescription(), GetRandomBoolean());
+}
---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Base/BaseFixture.cs
@@ -0,0 +1,23 @@
+﻿
+using Bogus;
+using FC.CodeFlix.Catalog.Infra.Data.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.Base;
+public class BaseFixture
+{
+    protected Faker Faker { get; set; }
+
+public BaseFixture() => Faker = new Faker("pt_BR");
+
+public CodeflixCatalogDbContext CreateDbContext()
+{
+    var context = new CodeflixCatalogDbContext(
+        new DbContextOptionsBuilder<CodeflixCatalogDbContext>()
+            .UseInMemoryDatabase("end2end-tests-db")
+            .Options
+    );
+
+    return context;
+}
+}
