1797a2650e11b87385f12a7871216cd72867c5d9
Douglas Emerick <douglas@emerick.net.br>
2022-04-28 09:13:28 -0300

    Criado o projeto da API

--- tests/FC.CodeFlix.Catalog.EndToEndTests/Base/ApiClient.cs
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Base/ApiClient.cs
@@ -13,14 +13,16 @@ public class ApiClient
         _httpClient = httpClient;
     }
 
-    public async Task<(HttpResponseMessage?, TOutput?)> Post<TOutput>(string route, object payload)
+    public async Task<(HttpResponseMessage?, TOutput?)> Post<TOutput>(string route, object payload) where TOutput : class
     {
         var response = await _httpClient.PostAsync(route, 
             new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json")
             );
 
         var outputString = await response.Content.ReadAsStringAsync();
-        var output = JsonSerializer.Deserialize<TOutput>(outputString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        TOutput? output = null;
+        if (!string.IsNullOrWhiteSpace(outputString))
+            output = JsonSerializer.Deserialize<TOutput>(outputString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         return (response, output);
     }
 }
