d2bbf0e9ea62d514be767e89a862efc5387c7b65
Douglas Emerick <douglas@emerick.net.br>
2022-04-29 17:49:45 -0300

    Setup da API

---
+++ src/FC.CodeFlix.Catalog.Api/Configurations/UseCasesConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using FC.CodeFlix.Catalog.Application.Interfaces;
+using FC.CodeFlix.Catalog.Application.UseCases.Category.CreateCategory;
+using FC.CodeFlix.Catalog.Domain.Repository;
+using FC.CodeFlix.Catalog.Infra.Data.EF;
+using FC.CodeFlix.Catalog.Infra.Data.EF.Repositories;
+using MediatR;
+
+namespace FC.CodeFlix.Catalog.Api.Configurations;
+
+public static class UseCasesConfiguration
+{
+    public static IServiceCollection AddUseCases(this IServiceCollection services)
+    {
+        services.AddMediatR(typeof(CreateCategory));
+        services.AddRepositories();
+        return services;
+    }
+
+    private static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        return services;
+    }
+}
