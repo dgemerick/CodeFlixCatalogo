fad2ff56cbb73fb1dc4cc15d7731a03e32d478e9
Douglas Emerick <douglas@emerick.net.br>
2022-05-03 22:32:25 -0300

    Criado teste buscar categoria pelo id

--- tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/Common/CategoryPersistence.cs
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/Common/CategoryPersistence.cs
@@ -1,6 +1,7 @@
 ﻿using FC.CodeFlix.Catalog.Infra.Data.EF;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using DomainEntity = FC.CodeFlix.Catalog.Domain.Entity;
 
@@ -14,6 +15,12 @@ public class CategoryPersistence
         _context = context;
     }
 
-    public async Task<DomainEntity.Category?> GetById(Guid id) 
+    public async Task<DomainEntity.Category?> GetById(Guid id)
         => await _context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+
+    public async Task InsertList(List<DomainEntity.Category> categories)
+    {
+        await _context.Categories.AddRangeAsync(categories);
+        await _context.SaveChangesAsync();
+    }
 }
---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/GetCategory/GetCategoryApiTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.CodeFlix.Catalog.EndToEndTests.Api.Category.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.Api.Category.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryApiTestFixture))]
+public class GetCategoryApiTestFixtureCollection : ICollectionFixture<GetCategoryApiTestFixture> { }
+
+public class GetCategoryApiTestFixture : CategoryBaseFixture
+{
+}
