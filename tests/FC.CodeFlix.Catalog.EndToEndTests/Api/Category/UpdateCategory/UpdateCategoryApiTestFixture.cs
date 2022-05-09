950094277deb2ddecb2caadda760332dfb4052fd
Douglas Emerick <douglas@emerick.net.br>
2022-05-09 20:38:53 -0300

    Criado teste do endpoint de update da category

---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using FC.CodeFlix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FC.CodeFlix.Catalog.EndToEndTests.Api.Category.Common;
+using System;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.Api.Category.UpdateCategory;
+
+[CollectionDefinition(nameof(UpdateCategoryApiTestFixture))]
+public class UpdateCategoryApiTestFixtureCollection : ICollectionFixture<UpdateCategoryApiTestFixture> { }
+
+public class UpdateCategoryApiTestFixture : CategoryBaseFixture
+{
+    public UpdateCategoryInput GetExampleInput(Guid? id = null)
+    {
+        return new UpdateCategoryInput(
+            id ?? Guid.NewGuid(),
+            GetValidCategoryName(),
+            GetValidCategoryDescription(),
+            GetRandomBoolean()
+        );
+    }
+}
--- tests/FC.CodeFlix.Catalog.EndToEndTests/Base/ApiClient.cs
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Base/ApiClient.cs
@@ -15,36 +15,46 @@ public class ApiClient
 
     public async Task<(HttpResponseMessage?, TOutput?)> Post<TOutput>(string route, object payload) where TOutput : class
     {
-        var response = await _httpClient.PostAsync(route, 
+        var response = await _httpClient.PostAsync(route,
             new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json")
             );
-
-        var outputString = await response.Content.ReadAsStringAsync();
-        TOutput? output = null;
-        if (!string.IsNullOrWhiteSpace(outputString))
-            output = JsonSerializer.Deserialize<TOutput>(outputString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        var output = await GetOutput<TOutput>(response);
         return (response, output);
     }
 
     public async Task<(HttpResponseMessage?, TOutput?)> Get<TOutput>(string route) where TOutput : class
     {
         var response = await _httpClient.GetAsync(route);
-
-        var outputString = await response.Content.ReadAsStringAsync();
-        TOutput? output = null;
-        if (!string.IsNullOrWhiteSpace(outputString))
-            output = JsonSerializer.Deserialize<TOutput>(outputString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        var output = await GetOutput<TOutput>(response);
         return (response, output);
     }
 
     public async Task<(HttpResponseMessage?, TOutput?)> Delete<TOutput>(string route) where TOutput : class
     {
         var response = await _httpClient.DeleteAsync(route);
+        var output = await GetOutput<TOutput>(response);
+        return (response, output);
+    }
+
+    public async Task<(HttpResponseMessage?, TOutput?)> Put<TOutput>(string route, object payload) where TOutput : class
+    {
+        var response = await _httpClient.PutAsync(route,
+            new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json")
+            );
+
+        var outputString = await response.Content.ReadAsStringAsync();
+        var output = await GetOutput<TOutput>(response);
+        return (response, output);
+    }
+
+    private async Task<TOutput?> GetOutput<TOutput>(HttpResponseMessage response) where TOutput : class
+    {
 
         var outputString = await response.Content.ReadAsStringAsync();
         TOutput? output = null;
         if (!string.IsNullOrWhiteSpace(outputString))
             output = JsonSerializer.Deserialize<TOutput>(outputString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-        return (response, output);
+
+        return output;
     }
 }
