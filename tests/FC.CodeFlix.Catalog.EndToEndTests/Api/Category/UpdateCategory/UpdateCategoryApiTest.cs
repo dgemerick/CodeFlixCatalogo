e504e1c8526320cd692a567430632c1663dd43f8
Douglas Emerick <douglas@emerick.net.br>
2022-05-09 20:48:10 -0300

    Criado teste UpdateCategoryNameAndDescription

--- tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -78,4 +78,35 @@ public class UpdateCategoryApiTest
         dbCategory.Description.Should().Be(exampleCategory.Description);
         dbCategory.IsActive.Should().Be(exampleCategory.IsActive!);
     }
+
+    [Fact(DisplayName = nameof(UpdateCategoryNameAndDescription))]
+    [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+    public async void UpdateCategoryNameAndDescription()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var exampleCategory = exampleCategoriesList[10];
+        var input = new UpdateCategoryInput(exampleCategory.Id, _fixture.GetValidCategoryName(), _fixture.GetValidCategoryDescription());
+
+        var (response, output) = await _fixture.ApiClient.Put<CategoryModelOutput>(
+            $"/categories/{exampleCategory.Id}",
+            input
+        );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Id.Should().Be(exampleCategory.Id);
+        output!.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive!);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+
+        var dbCategory = await _fixture.Persistence.GetById(exampleCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(input.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive!);
+    }
 }
