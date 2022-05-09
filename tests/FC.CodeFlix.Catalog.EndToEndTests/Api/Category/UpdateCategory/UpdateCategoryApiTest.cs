5c581e25aa961558725ca6a6a79573d55f9b1308
Douglas Emerick <douglas@emerick.net.br>
2022-05-09 20:46:56 -0300

    Criado teste UpdateCategoryOnlyName

--- tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/UpdateCategory/UpdateCategoryApiTest.cs
@@ -1,4 +1,5 @@
 ﻿using FC.CodeFlix.Catalog.Application.UseCases.Category.Common;
+using FC.CodeFlix.Catalog.Application.UseCases.Category.UpdateCategory;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using System.Net;
@@ -45,5 +46,36 @@ public class UpdateCategoryApiTest
         dbCategory!.Name.Should().Be(input.Name);
         dbCategory.Description.Should().Be(input.Description);
         dbCategory.IsActive.Should().Be((bool)input.IsActive!);        
+    }
+
+    [Fact(DisplayName = nameof(UpdateCategoryOnlyName))]
+    [Trait("EndToEnd/API", "Category/Update - Endpoints")]
+    public async void UpdateCategoryOnlyName()
+    {
+        var exampleCategoriesList = _fixture.GetExampleCategoriesList(20);
+        await _fixture.Persistence.InsertList(exampleCategoriesList);
+        var exampleCategory = exampleCategoriesList[10];
+        var input = new UpdateCategoryInput(exampleCategory.Id, _fixture.GetValidCategoryName());
+
+        var (response, output) = await _fixture.ApiClient.Put<CategoryModelOutput>(
+            $"/categories/{exampleCategory.Id}",
+            input
+        );
+
+        response.Should().NotBeNull();
+        response!.StatusCode.Should().Be((HttpStatusCode)StatusCodes.Status200OK);
+        output.Should().NotBeNull();
+        output!.Id.Should().Be(exampleCategory.Id);
+        output!.Name.Should().Be(input.Name);
+        output.Description.Should().Be(exampleCategory.Description);
+        output.IsActive.Should().Be(exampleCategory.IsActive!);
+        output.Id.Should().NotBeEmpty();
+        output.CreatedAt.Should().NotBeSameDateAs(default);
+
+        var dbCategory = await _fixture.Persistence.GetById(exampleCategory.Id);
+        dbCategory.Should().NotBeNull();
+        dbCategory!.Name.Should().Be(input.Name);
+        dbCategory.Description.Should().Be(exampleCategory.Description);
+        dbCategory.IsActive.Should().Be(exampleCategory.IsActive!);
     }
 }
