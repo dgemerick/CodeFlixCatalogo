f3d3815342ef7c7cece88d20e7b22370db95d839
Douglas Emerick <douglas@emerick.net.br>
2022-05-11 19:33:18 -0300

    Criado teste do endpoint de listagem de categorias

---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Api/Category/ListCategories/ListCategoriesApiTestFixture.cs
@@ -0,0 +1,11 @@
+﻿using FC.CodeFlix.Catalog.EndToEndTests.Api.Category.Common;
+using Xunit;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.Api.Category.ListCategories;
+
+[CollectionDefinition(nameof(ListCategoriesApiTestFixture))]
+public class ListCategoriesApiTestFixtureCollection : ICollectionFixture<ListCategoriesApiTestFixture> { }
+
+public class ListCategoriesApiTestFixture : CategoryBaseFixture
+{
+}
