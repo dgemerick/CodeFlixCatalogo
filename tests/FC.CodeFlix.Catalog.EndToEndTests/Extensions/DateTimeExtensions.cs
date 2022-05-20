5f815cb1d8dcc7dbf66f436669124aa9d8562f18
Douglas Emerick <douglas@emerick.net.br>
2022-05-20 20:43:58 -0300

    Criado Classe DateTimeExtensions

---
+++ tests/FC.CodeFlix.Catalog.EndToEndTests/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FC.CodeFlix.Catalog.EndToEndTests.Extensions;
+internal static class DateTimeExtensions
+{
+    public static DateTime TrimMillisseconds(this DateTime dateTime)
+    {
+        return new DateTime(
+            dateTime.Year,
+            dateTime.Month,
+            dateTime.Day,
+            dateTime.Hour,
+            dateTime.Minute,
+            dateTime.Second,
+            0,
+            dateTime.Kind
+        );
+
+    }
+        
+}
